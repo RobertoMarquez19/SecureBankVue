59d8132219d637e8db02971bca9a9771a92866f8
kath-cas <kathalina.vadimof@gmail.com>
2023-06-12 17:10:01 -0600

    Diseño Home

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -3,22 +3,33 @@
         <div class="row">
             <div class="col-12">
                 <div class="card shadow-sm">
-                    <div class="card-header">
-                        <h3>Dashboard</h3>
+                    <div class="card-header text-center">
+                        <img src="images/sinslogan2.png" style="width: 180px;" alt="logo">
                     </div>
                     <div class="card-body">
-                        <p class="mb-0">Iniciaste Sesion como: <b>{{this.auth.user.correoElectronico}}</b></p>
+                        <p class="mb-0">Iniciaste Sesion como: <b>{{ this.auth.user.correoElectronico }}</b></p>
+                    </div>
+                </div>
+                <br>
+                <div class="col-xxl-11 mx-auto p-2">
+                    <div class="card shadow-lg">
+                        <div class="card-body ">
+                            <img src="images/img2.png" alt="logo" class="rounded float-start img-thumbnail">
+                            <img src="images/img1.png" alt="logo" class="rounded float-end img-thumbnail">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <br>
+
     </div>
 </template>
 
 <script>
 export default {
-    name:"dashboard",
-    data(){
+    name: "dashboard",
+    data() {
         return {
             user: this.auth.user
         }
--- resources/js/components/layouts/Default.vue
+++ resources/js/components/layouts/Default.vue
@@ -1,47 +1,46 @@
 <template>
     <div class="container-fluid">
         <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <span href="#"
-                          class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span class="fs-5 d-none d-sm-inline">Secure Bank</span>
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 color1">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 ">
+                    <span href="#" class="mx-auto p-2" >
+                        <img src="images/icono.png" style="width: 100px;" alt="logo">
                     </span>
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                        id="menu">
-                        <li class="nav-item">
-                            <router-link :to="{name:'dashboard'}" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item ">
+                            <router-link :to="{ name: 'dashboard' }" class="nav-link align-middle px-0">
+                                <i class="fs-2 bi bi-house color2"></i> <span
+                                    class="ms-1 d-none d-sm-inline text3">Home</span>
                             </router-link>
                         </li>
-                        <li class="nav-item">
-                            <router-link :to="{name:'cuentas'}" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi bi-cash-coin"></i> <span
-                                class="ms-1 d-none d-sm-inline">Cuentas</span>
+                        <li class="nav-item ">
+                            <router-link :to="{ name: 'cuentas' }" class="nav-link align-middle px-0">
+                                <i class="fs-2 bi bi-cash-coin color2"></i>
+                                <span class="ms-1 d-none d-sm-inline text3">Cuentas</span>
                             </router-link>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi bi-credit-card-2-back"></i> <span class="ms-1 d-none d-sm-inline">Tarjetas</span>
+                                <i class="fs-2 bi bi-credit-card-2-back color2"></i>
+                                <span class="ms-1 d-none d-sm-inline text3">Tarjetas</span>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="#" class="nav-link align-middle px-0">
-                                <i class="fs-4 bi bi-currency-bitcoin"></i> <span
-                                class="ms-1 d-none d-sm-inline">Crypto</span>
+                                <i class="fs-2 bi bi-currency-bitcoin color2"></i> <span
+                                    class="ms-1 d-none d-sm-inline text3">Crypto</span>
                             </a>
                         </li>
                     </ul>
                     <hr>
                     <div class="dropdown pb-4">
-                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                           id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="images/profile.png" alt="Profile Picture" width="30" height="30"
-                                 class="rounded-circle">
-                            <span
-                                class="d-none d-sm-inline mx-1 text-wrap">{{ this.user.nombres }} {{ this.user.apellidos }}</span>
+                        <a href="#" class="d-flex align-items-center text3 text-decoration-none dropdown-toggle"
+                            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="images/user1.png" alt="Profile Picture" width="25" height="26">
+                            <span class="d-none d-sm-inline mx-1 text-wrap ">{{ this.user.nombres }} {{ this.user.apellidos
+                            }}</span>
                         </a>
-                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser1">
                             <li>
                                 <a class="dropdown-item" href="javascript:void(0)" @click="logout">Logout</a>
                             </li>
@@ -56,7 +55,6 @@
             </div>
         </div>
     </div>
-
 </template>
 
 <script>
@@ -98,7 +96,7 @@ export default {
                         console.log(response);
                         let tokenResponse = response.data.data;
                         Auth.renewToken(tokenResponse);
-                    }).catch(({response}) => {
+                    }).catch(({ response }) => {
                         console.error(response.data);
                         let mensajeError = response.data.message;
                         if (response.status === 401 || response.status === 422) {
@@ -131,7 +129,7 @@ export default {
             await axios.post('cliente/sesion/logout').then((response) => {
                 console.log(response);
                 Auth.logout();
-            }).catch(({response}) => {
+            }).catch(({ response }) => {
                 console.error(response.data);
                 let mensajeError = response.data.message;
                 if (response.status === 401 || response.status === 422) {
@@ -156,6 +154,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
